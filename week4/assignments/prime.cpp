8806b327dc7eb069e4a373d19a497d141be4296e
Jacob Ogden <ogdenj@ncmich.edu>
2022-10-10 13:47:21 -0400

    a

--- week4/assignments/prime.cpp
+++ week4/assignments/prime.cpp
@@ -1,3 +1,4 @@
+// Jacob Ogden
 #include <iostream>
 using namespace std;
 
@@ -5,11 +6,14 @@ bool is_prime(int n) {
     int x;
     bool prime = true;
 
+    // Check for zero and ones.
     if (n == 0 || n == 1) {
         prime = false;
     }
 
+    // Then we start looping from 2 until x exceeds half of n.
     for (x = 2; x <= n / 2; ++x) {
+        // Check if the modulus of n and x equals zero, break if true.
         if (n % x == 0) {
             prime = false;
             break;
@@ -21,12 +25,8 @@ bool is_prime(int n) {
 
 
 int main() {
-    for (
-        int x = 0;
-        x >= 100;
-        x++
-        ) {
-        cout << x << " " << is_prime(x) << endl;
+    // Loop 100 times, feeding x into is_prime and outputting the result to console.
+    for (int x = 0; x <= 100; x++) {
         if (is_prime(x)) {
             cout << x << " is prime." << endl;
         }
