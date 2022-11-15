d10f8e18931683de492646c8cc1e4dacde6f61b4
Jacob Ogden <ogdenj@ncmich.edu>
2022-11-14 19:15:00 -0500

    a

--- week9/examples/dynamic1.cpp
+++ week9/examples/dynamic1.cpp
@@ -6,27 +6,26 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-  int var = 0;
+int main(int argc, char *argv[]) {
+    int var = 0;
 
-  
-  int *intPtr = new int;                        // keyword new
-                                                          // allocate enough memory for a new int
-                                                          // variable being created.  
-  
-  *intPtr = 25;
-  
-  cout << "Pointer Value: " << *intPtr << endl << endl;
-  
-  cout << "Enter an Integer Value: ";
-  
-  
-  cin >> var;
-  
-  *intPtr = *intPtr + var; 
-  
-  cout << "intPtr + " << var  << " = "  << *intPtr << endl;
-  
-  return 0;
+
+    int *intPtr = new int;                      // keyword new
+    // allocate enough memory for a new int
+    // variable being created.  
+
+    *intPtr = 25;
+
+    cout << "Pointer Value: " << *intPtr << endl << endl;
+
+    cout << "Enter an Integer Value: ";
+
+
+    cin >> var;
+
+    *intPtr = *intPtr + var;
+
+    cout << "intPtr + " << var << " = " << *intPtr << endl;
+
+    return 0;
 }
