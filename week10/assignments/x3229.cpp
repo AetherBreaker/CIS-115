7c3165acd1d927a19bab11c2112018f912d0dc78
Jacob Ogden <Jacob.ogden@yahoo.com>
2022-11-21 16:37:46 -0500

    a

--- week10/assignments/x3229.cpp
+++ week10/assignments/x3229.cpp
@@ -9,8 +9,21 @@ using std::unique_ptr;
 
 
 int main() {
-    // Write a List of pointers that deletes the objects pointed to when it itself is destroyed (and must be removed from the list) or if the element is removed from the List using unique_ptr.
+    // using unique_ptr, Write a List of pointers that deletes the objects pointed to when it itself is destroyed
+    // (and must be removed from the list)
+    // or if the element is removed from the List .
+
     list<unique_ptr<int>> l;
+    l.push_back(unique_ptr<int>(new int(1)));
+    l.push_back(unique_ptr<int>(new int(2)));
+    l.push_back(unique_ptr<int>(new int(3)));
+    l.push_back(unique_ptr<int>(new int(4)));
+    l.push_back(unique_ptr<int>(new int(5)));
+
+    for (auto i : l) {
+        cout << *i << endl;
+    }
+
 
 
 }
