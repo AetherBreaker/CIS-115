d23356da2ed9220ba3ece584109ad3756ef68ec2
Jacob Ogden <Jacob.ogden@yahoo.com>
2023-05-11 16:35:28 -0400

    a

--- week7/examples/file1.cpp
+++ week7/examples/file1.cpp
@@ -2,22 +2,22 @@
 // Opens a file...
 
 #include<iostream>
-#include<fstream>
+#include<fstream>
+
 using namespace std;
 
-int main()
-{
-	char fileName[8];
-	fstream dataFile;  					// Declare a file stream object
+int main() {
+    char fileName[8];
+    fstream dataFile;  					// Declare a file stream object
 
-	cout << "Enter the file name you wish to open or create: " << endl;
+    cout << "Enter the file name you wish to open or create: " << endl;
 
-	cin  >> fileName;
+    cin >> fileName;
 
-	dataFile.open(fileName,ios::out);  		//note file mode flag
-	
-	// the file is created in the same folder as this source code is in.
-	// (assuming you have the proper permissions)
+    dataFile.open(fileName, ios::out);  		//note file mode flag
 
-	return 0;
+    // the file is created in the same folder as this source code is in.
+    // (assuming you have the proper permissions)
+
+    return 0;
 }
